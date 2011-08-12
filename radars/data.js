b0bce32767642270a2db9bacc908016d8fe5d53f
filipesperandio <filipesperandio>
2011-08-12 11:24:07 -0300

    radar on july 2011

--- radars/data.js
+++ radars/data.js
@@ -1,33 +1,47 @@
-title = 'Your Name, Month Year';
+title = 'Filipe Esperandio, July 2011';
 
 data = {
-    'First Quadrant': {
-        'Very High Skill': 4,
-        'High Skill': 3,
-        'Medium Skill': 2,
-        'Low Skill': 1,
+    'Languages': {
+        'Java': 4,
+        'Javascript' : 2,
+        'Python': 1,
+        'Ruby': 1,
+        'Shell Script': 3,
     },
 
-    'Second Quadrant': {
-        'Very High Skill': 4,
-        'High Skill': 3,
-        'Medium Skill': 2,
-        'Low Skill': 1,
+    'Tools': {
+        'Eclipse':4,
+        'Git':3,
+        'EC2':2,
+        'Ivy':3,
+        'Ant':3,
+        'Vim':2,
+        'Svn':4,
+        'IntelliJ':1,
+        'Maven':1,
+        'Rake': 1,
     },
 
-    'Third Quadrant': {
-        'Very High Skill': 4,
-        'High Skill': 3,
-        'Medium Skill': 2,
-        'Low Skill': 1,
+    'Techniques': {
+        'TDD': 3,
+        'Design Patterns': 2,
+        'DDD': 2,
+        'BDD': 1,
+        'OOD': 4,
+        'DevOps':3,
+        'Refactoring':3,
     },
 
-    'Fourth Quadrant': {
-        'Very High Skill': 4,
-        'High Skill': 3,
-        'Medium Skill': 2,
-        'Low Skill': 1,
-    },
-
+    'Platforms': {
+        'Linux':4,
+        'Windows':3,
+    	'ESX/vSphere':3,
+        'Mac OSX': 1,
+        'Network stuffs': 3,
+        'Cisco': 2,
+        'Android': 2,
+        'iOS': 1,
+    }
 };
+
 
