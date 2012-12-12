8d5a1a14c53ae91735f205191bce4aa67ad61f09
Filipe Gomes Esperandio <filipesperandio@gmail.com>
2012-12-11 16:35:29 -0800

    replace techniques by framworks

--- radars/data.js
+++ radars/data.js
@@ -12,29 +12,25 @@ data = {
   'Tools': {
     'Eclipse':4,
     'Git/Svn':4,
-    'VRaptor/Restfulie':4,
-    'Mokito/Jmock':4,
-    'AngularJS':3,
-    'Jasmine':3,
-    'Bootstrap':3,
     'Vim':3,
-    'Ant/Maven/Ivy':3,
-    'Axis2/Spring/Hibernate/IBatis':3,
+    'Maven/Ant':3,
     'Grunt':2,
     'Node':2,
-    'jQuery':2,
     'Oracle':2,
     'MongoDB':2,
-    'SoapUI':2
+    'Curl/Httpie': 2
   },
 
-  'Techniques': {
-    'DDD': 3,
-    'TDD': 3,
-    'DevOps':3,
-    'Refactoring':3,
-    'OOD': 4,
-    'BDD': 1
+  'Frameworks': {
+    'AngularJS': 4,
+    'jQuery':2,
+    'Jasmine':3,
+    'Bootstrap':4,
+    'VRaptor/Restfulie':4,
+    'Mokito':4,
+    'Spring':3,
+    'Hibernate':3,
+    'Rails': 2
   },
 
   'Platforms': {
